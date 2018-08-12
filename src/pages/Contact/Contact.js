e711224f95bf829091e6ca48c5c7fe9f7ec6ae18
ektravel <38017980+ektravel@users.noreply.github.com>
2018-08-12 01:07:49 -0500

    finished Contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -13,8 +13,30 @@ class Contact extends Component {
     return (
     <div className="container">
         <div className="row">
-            <div className="col-md-12" id="homeContainer">
-            This is the Contact page.
+            <div className="col-md-6">
+            
+            <div className="card">
+                <h4 className="card-header bg-transparent text-center" id="contactHeader">Let's connect!</h4>
+                <div className="card-body bg-transparent">
+                    <form method="POST" action="https://formspree.io/kmpromoatx@gmail.com">
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input name="name" className="form-control" type="text" placeholder="Your name"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input name="email" className="form-control" type="text" placeholder="Your email"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="notes">Message</label>
+                            <textarea name="message" className="form-control" rows="3" type="text" placeholder="Your message"></textarea>
+                        </div>
+                        <button type="submit" className="btn">Submit</button>
+                    </form>
+                </div>
+            </div>
+
+
             </div>
         </div>
     </div>
