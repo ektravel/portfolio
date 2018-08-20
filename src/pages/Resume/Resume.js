f32ac081df7869db60d3149dd92487b010f09dbb
ektravel <38017980+ektravel@users.noreply.github.com>
2018-08-20 16:24:23 -0500

    fixed link color issue

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -17,8 +17,8 @@ class Resume extends Component {
           <div className="col-md-12">
           {/* if Link fails to open pdf, use a href */}
           {/* <a href="/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener">Click here to view my resume.</a> */}
-            <span class="border border-light">
-              <Link to = "/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener">Click here to view my resume.</Link>
+            <span class="border border-light" id="resumeLink">
+              <Link to = "/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener" >Click here to view my resume</Link>
             </span>
           </div>
         </div>
