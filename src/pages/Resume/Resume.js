7383cd667e36887b5f368fcde2787dbf53a4a4d2
ektravel <38017980+ektravel@users.noreply.github.com>
2018-09-15 15:04:53 -0500

    fixed className issue

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -17,7 +17,7 @@ class Resume extends Component {
           <div className="col-md-12">
           {/* if Link fails to open pdf, use a href */}
           {/* <a href="/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener">Click here to view my resume.</a> */}
-            <span class="border border-light" >
+            <span className="border border-light" >
               <Link to = "/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener" id="resumeLink">Click here to view my resume</Link>
             </span>
           </div>
