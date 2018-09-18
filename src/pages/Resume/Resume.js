7d793d01e1a716de99f3e8af2db540a9179eb76d
ektravel <38017980+ektravel@users.noreply.github.com>
2018-09-18 16:27:36 -0500

    code to fix background image bug

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -17,7 +17,7 @@ class Resume extends Component {
           <div className="col-md-12">
             <span className="border border-light" >
               <Link to = "/assets/E_Macedo_Resume.pdf" target="_blank" rel="noreferrer noopener" id="resumeLink">Click to display resume</Link>
-            </span>
+            </span> 
           </div>
         </div>
       </div>
